5f5af6e55e7c4b94e6409046b718bae822a1e076
Stre4K <eric.strihagen@gmail.com>
2024-05-01 17:26:07 +0200

    Fixed dependencies for app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd, lookupExtended
+from helpers import apology, login_required
 
 import pandas as pd
 import plotly.express as px
@@ -13,9 +13,6 @@ import pandas_market_calendars as mcal
 
 # Configure application
 app = Flask(__name__)
-
-# Custom filter
-app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False

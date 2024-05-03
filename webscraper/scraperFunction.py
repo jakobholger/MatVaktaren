229af6c9341a6cd5b29e53358eb3caec3c26f391
jakobholger <jakob.akervall@gmail.com>
2024-05-03 21:00:27 +0200

    Fixed spelling

--- webscraper/scraperFunction.py
+++ webscraper/scraperFunction.py
@@ -1,13 +1,14 @@
 import requests
 import json
 from dbFunctions import create_product, add_price_for_product
-from datetime import datetime
+from datetime import datetime, timedelta
+import random
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
 }
 
-pushed_date = datetime.now().date()
+pushed_date = datetime.now().date() + timedelta(days=4)
 
 def scrape(productCode):
 #url = 'https://www.willys.se/_next/data/6812123c/sv.json?q=%C3%A4gg&name=agg-24p-Frigaende-Inomhus-Medium-101187348_ST&productCode=101187348_ST'
@@ -35,9 +36,10 @@ def parse_html(html_content, productCode):
     #    print(json_data['potentialPromotions'][0]['lowestHistoricalPrice'])
     priceCurrency = json_data["price"].split(" ")
 
-    json_data["price"] = priceCurrency[0]
+    json_data["price"] = priceCurrency[0].replace(",", ".")
     json_data["currency"] = priceCurrency[1]
 
     print(json_data['name'], "|", json_data['price'], json_data['currency'], "/", json_data['displayVolume'], "|", json_data['comparePrice'], "/", json_data['comparePriceUnit'])
+
     create_product(json_data['name'], json_data['displayVolume'], productCode, json_data['googleAnalyticsCategory'])
-    add_price_for_product(json_data['name'], json_data['price'], json_data['currency'], pushed_date, json_data['comparePrice'] + "/" + json_data['comparePriceUnit'], productCode)
+    add_price_for_product(json_data['name'], random.randint(15, 200), json_data['currency'], pushed_date, json_data['comparePrice'] + "/" + json_data['comparePriceUnit'], productCode)
